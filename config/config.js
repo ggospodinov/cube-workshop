473b111e6f0a0713f1385d5a3ea0bc6d58180b96
ggospodinov <baigrozdan99@abv.bg>
2020-06-07 09:31:10 +0300

    feat: migrate database to mongodb

--- config/config.js
+++ config/config.js
@@ -1,6 +1,8 @@
 module.exports = {
     development: {
-        port: process.env.PORT || 3000
+        port: process.env.PORT || 3000,
+       
+        
     },
     production: {}
 };
--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const env = process.env.NODE_ENV || 'development';
-
+const mongoose = require('mongoose')
 const config = require('./config/config')[env];
+
+
 const express = require('express');
 const indexRouter = require('./routes')
-const app= express();
+const app = express();
+
+
+const url=`mongodb+srv://user:softuni@softuni-ivota.mongodb.net/<cubecle>?retryWrites=true&w=majority`
+mongoose.connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log('MongoDB Connected…')
+  })
+  .catch(err => console.log(err))
 
 require('./config/express')(app);
 app.use('/', indexRouter)
